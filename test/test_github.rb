8dc650ec9ef5f56e1bfa7c998af280cb26075406
Sebastian Staudt <koraktor@gmail.com>
2011-06-05 22:18:01 +0200

    Added some more tests

--- test/test_github.rb
+++ test/test_github.rb
@@ -9,11 +9,19 @@ class TestGitHub < Test::Unit::TestCase
 
   context 'The GitHub implementation' do
 
+    should 'not support file stats' do
+      assert_not Metior::GitHub.supports? :file_stats
+    end
+
     should 'not support line stats' do
       assert_not Metior::GitHub.supports? :line_stats
-      object = Object.new
-      object.extend Metior::GitHub
-      assert_not object.supports? :line_stats
+    end
+
+    should 'not be able to get file stats of a repository' do
+      repo = Metior::GitHub::Repository.new 'koraktor', 'rubikon'
+      assert_raises UnsupportedError do
+        repo.file_stats
+      end
     end
 
     should 'not be able to get the most significant authors of a repository' do
---
+++ test/test_vcs.rb
@@ -0,0 +1,38 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2011, Sebastian Staudt
+
+require 'helper'
+
+class TestVCS < Test::Unit::TestCase
+
+  context 'The VCS module' do
+
+    setup do
+      module MockVCS
+        NAME = :mock
+        include VCS
+        not_supported :some_feature
+      end
+
+      @vcs = Object.new
+      @vcs.extend MockVCS
+    end
+
+    should 'allow access to the features of a VCS' do
+      assert_not MockVCS.supports? :some_feature
+      assert_not @vcs.supports? :some_feature
+      assert_raise UnsupportedError do
+        @vcs.support! :some_feature
+      end
+    end
+
+    teardown do
+      @vcs = nil
+      Metior.module_exec { @@vcs_types.delete :mock }
+    end
+
+  end
+
+end
