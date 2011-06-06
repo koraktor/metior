91479c700f7735c02a194434ce8410df8b0dde1e
Sebastian Staudt <koraktor@gmail.com>
2011-06-06 06:53:59 +0200

    Added some basic Repository tests

---
+++ test/test_repository.rb
@@ -0,0 +1,28 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2011, Sebastian Staudt
+
+class TestRepository < Test::Unit::TestCase
+
+  context 'The base class for Metior VCS repositories' do
+
+    setup do
+      @repo = Metior::Repository.new('dummy')
+    end
+
+    should 'not implement the #load_commits method' do
+      assert_raise NotImplementedError do
+        @repo.send(:load_commits, nil)
+      end
+    end
+
+    should 'parse commit ranges correctly' do
+      assert_equal 'master'..'development', @repo.send(:parse_range, 'master'..'development')
+      assert_equal 'master'..'development', @repo.send(:parse_range, 'master..development')
+      assert_equal ''..'master', @repo.send(:parse_range, 'master')
+    end
+
+  end
+
+end
