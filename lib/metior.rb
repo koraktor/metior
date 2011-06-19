8bd92ffac638d4492188ae52a8b159936d46504f
Sebastian Staudt <koraktor@gmail.com>
2011-06-19 14:18:40 +0200

    Really really fix .simple_stats for GitHub
    Just don't use parameter globbing for that method. That can never work
    reliably.

--- lib/metior.rb
+++ lib/metior.rb
@@ -27,23 +27,23 @@ module Metior
   # Calculates simplistic stats for the given repository and branch
   #
   # @param [Symbol] type The type of the repository, e.g. `:git`
-  # @param [Array<Object>] options The options for the repository. The last one
-  #        should be the range of commits for which the commits should be
-  #        loaded. This may be given as a string (`'master..development'`), a
-  #        range (`'master'..'development'`) or as a single ref (`'master'`). A
-  #        single ref name means all commits reachable from that ref.
-  # @return [Hash] The calculated stats for the given repository and branch
-  def self.simple_stats(type, *options)
-    arity  = vcs(type)::Repository.instance_method(:initialize).arity
-    arity = (arity < 0) ? -(arity + 1) : arity
-    branch = options.delete_at arity
-    branch = vcs(type)::DEFAULT_BRANCH if branch.nil?
-    repo   = repository type, *options
+  # @param [Object, Array<Object>] repo_options The options to supply to the
+  #        repository
+  # @param [String, Range] range The range of commits for which the commits
+  #        should be loaded. This may be given as a string
+  #        (`'master..development'`), a range (`'master'..'development'`) or as
+  #        a single ref (`'master'`). A single ref name means all commits
+  #        reachable from that ref.
+  # @return [Hash<Symbol, Object>] The calculated stats for the given
+  #         repository and branch
+  def self.simple_stats(type, repo_options, range = nil)
+    range = vcs(type)::DEFAULT_BRANCH if range.nil?
+    repo  = repository type, *repo_options
 
-    commits = repo.commits(branch)
+    commits = repo.commits(range)
     {
       :commit_count     => commits.size,
-      :top_contributors => repo.top_contributors(branch, 5),
+      :top_contributors => repo.top_contributors(range, 5),
     }.merge Commit.activity(commits)
   end
 
