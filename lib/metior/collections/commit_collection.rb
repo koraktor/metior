e3843b9ba265d94247b3436edbcf1438fef38cfb
Sebastian Staudt <koraktor@gmail.com>
2011-06-26 09:24:17 +0200

    Added a method to filter a commit collection by author

--- lib/metior/collections/commit_collection.rb
+++ lib/metior/collections/commit_collection.rb
@@ -31,6 +31,24 @@ module Metior
       authors
     end
 
+    # Returns the list of commits that have been authored by the given authors
+    #
+    # @param [Array<Actor, Object>] author_ids One or more actual `Actor`
+    #        instances or IDs of the authors that the commits should be
+    #        filtered by
+    # @return [CommitCollection] The commits that have been authored by the
+    #         given authors
+    def by(*author_ids)
+      commits = CommitCollection.new
+      author_ids.flatten.each do |author_id|
+        author_id = author_id.id if author_id.is_a? Actor
+        each_value do |commit|
+          commits << commit if commit.author.id == author_id
+        end
+      end
+      commits
+    end
+
     # Returns the committers of all or a specific commit in this collection
     #
     # @param [Object] commit_id The ID of the commit, if only the committer of
