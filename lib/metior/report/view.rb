21e38ba5513812ef30d96e7c54347aa6f97cad37
Sebastian Staudt <koraktor@gmail.com>
2011-08-25 16:11:59 +0200

    Cleanup metior/report/view.rb

--- lib/metior/report/view.rb
+++ lib/metior/report/view.rb
@@ -122,8 +122,7 @@ class Metior::Report
     # @see http://rubydoc.info/gems/mustache/Mustache#view_class-class_method
     #      Mustache.view_class
     def respond_to?(name)
-      methods.include?(name.to_s) ||
-      repository.respond_to?(name)
+      methods.include?(name.to_s) || repository.respond_to?(name)
     end
 
     # Returns the name of the VCS the analyzed repository is using
@@ -136,26 +135,3 @@ class Metior::Report
   end
 
 end
-
-#class Mustache::Context
-#
-#  def find(obj, key, default = nil)
-#      hash = obj.respond_to?(:has_key?)
-#
-#      if hash && obj.has_key?(key)
-#        obj[key]
-#      elsif hash && obj.has_key?(key.to_s)
-#        obj[key.to_s]
-#      elsif !obj.instance_of?(Hash) && obj.respond_to?(key)
-#        meth = obj.method(key) rescue proc { obj.send(key) }
-#        if meth.arity == 1
-#          meth.to_proc
-#        else
-#          meth[]
-#        end
-#      else
-#        default
-#      end
-#    end
-#
-#end
